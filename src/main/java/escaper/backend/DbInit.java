533e59bb6b7d0d063fade87196c6144752d51e7c
wooho <woohobi@gmail.com>
2022-05-05 16:23:13 +0200

    Chore db 초기데이터 변경

--- src/main/java/escaper/backend/DbInit.java
+++ src/main/java/escaper/backend/DbInit.java
@@ -1,6 +1,7 @@
 package escaper.backend;
 
 import escaper.backend.entity.Post;
+import escaper.backend.entity.Theme;
 import escaper.backend.entity.User;
 import escaper.backend.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
@@ -36,19 +37,27 @@ public class DbInit {
             User user2 = new User("user2");
             em.persist(user1);
             em.persist(user2);
-            Post post1 = createPost("dbinit", "test", user1);
+            Theme theme1 = new Theme("숨겨진 별장의 비밀");
+            Theme theme2 = new Theme("우주로 나간 아이");
+            Theme theme3 = new Theme("몬스터");
+            em.persist(theme1);
+            em.persist(theme2);
+            em.persist(theme3);
+            Post post1 = createPost("post1", "test", user1, theme1);
             postRepository.save(post1);
 
-            Post post2 = createPost("dbinit2", "test2", user1);
+            Post post2 = createPost("post2", "test2", user1,theme2);
             postRepository.save(post2);
 
 
-            Post post3 = createPost("dbinit3", "test3", user2);
+            Post post3 = createPost("post3", "test3", user2, theme3);
             postRepository.save(post3);
+            Post post4 = createPost("post4", "test4", user2, theme2);
+            postRepository.save(post4);
         }
 
-        private Post createPost(String title, String content, User user) {
-            return new Post(title, content, user);
+        private Post createPost(String title, String content, User user, Theme theme) {
+            return new Post(title, content, user, theme);
         }
     }
 }
