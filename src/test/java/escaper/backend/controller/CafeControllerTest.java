9c7b820c9d6264f0344b1500d57b1665bac55b76
wooho <woohobi@gmail.com>
2022-05-06 21:20:43 +0200

    Test 지역 api controller

---
+++ src/test/java/escaper/backend/controller/CafeControllerTest.java
@@ -0,0 +1,31 @@
+package escaper.backend.controller;
+
+import escaper.backend.repository.cafe.CafeRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class CafeControllerTest {
+
+    @Mock
+    private CafeRepository cafeRepository;
+    @InjectMocks
+    private CafeController cafeController;
+
+    @Test
+    @DisplayName("모든 카페 이름을 리턴한다")
+    public void getCafeName() {
+        //given
+        cafeController.getAddress();
+        //when
+        //then
+        verify(cafeRepository).getAddress();
+    }
+
+}
