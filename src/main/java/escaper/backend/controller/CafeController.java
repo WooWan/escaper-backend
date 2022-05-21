fe12961da600f4a0e7a29e94260ce539a27b78bf
wooho <woohobi@gmail.com>
2022-05-21 23:28:57 +0200

    Feat search api 완성

--- src/main/java/escaper/backend/controller/CafeController.java
+++ src/main/java/escaper/backend/controller/CafeController.java
@@ -1,17 +1,16 @@
 package escaper.backend.controller;
 
-import escaper.backend.entity.AddressDto;
-import escaper.backend.entity.Cafe;
+import escaper.backend.entity.cafe.*;
 import escaper.backend.repository.cafe.CafeRepository;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Getter
@@ -21,21 +20,19 @@ public class CafeController {
 
     private final CafeRepository cafeRepository;
 
-    @GetMapping("/api/city")
-    public List<String> getCity() {
-        return cafeRepository.getCity();
+    @GetMapping("/api/cities")
+    public List<CitySearchDto> getCity() {
+        return cafeRepository.searchCity();
     }
 
-    @GetMapping("/api/area")
-    public List<String> getArea(@RequestParam(required = false) String city) {
-        return cafeRepository.getAreaByCity(city);
+    @GetMapping("/api/areas")
+    public List<AreaSearchDto> searchArea(@RequestParam(required = false) String city) {
+        return cafeRepository.searchArea(city);
     }
 
-    @GetMapping("/api/address")
-    public List<AddressDto> getAddress() {
-        List<Cafe> cafeList = cafeRepository.findAll();
-        return cafeList.stream()
-                .map(AddressDto::new)
-                .collect(Collectors.toList());
+    @GetMapping("/api/cafes")
+    public List<CafeSearchDto> searchCafe(@RequestParam(required = false) String area) {
+        return cafeRepository.searchCafe(area);
     }
+
 }
