844e86d990ffab3707390e865756c8bad3f76010
wooho <woohobi@gmail.com>
2022-05-22 13:35:08 +0200

    Feat search city api

--- src/main/java/escaper/backend/repository/cafe/CafeRepository.java
+++ src/main/java/escaper/backend/repository/cafe/CafeRepository.java
@@ -1,7 +1,14 @@
 package escaper.backend.repository.cafe;
 
-import escaper.backend.entity.Cafe;
+import escaper.backend.entity.cafe.Cafe;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface CafeRepository extends JpaRepository<Cafe, Integer>, CafeRepositoryCustom {
+
+    @Query("SELECT DISTINCT c.address.city FROM Cafe c")
+    List<String> searchCities();
+
 }
