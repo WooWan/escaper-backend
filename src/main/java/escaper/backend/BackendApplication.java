131dccd8b571ec610a9c50f0769c025b218e6dea
wooho <woohobi@gmail.com>
2022-05-22 13:27:56 +0200

    Add jpa auditing

--- src/main/java/escaper/backend/BackendApplication.java
+++ src/main/java/escaper/backend/BackendApplication.java
@@ -1,11 +1,16 @@
 package escaper.backend;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.persistence.EntityManager;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BackendApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
