cf77122304b1e7162fbd90839a669e477be8f812
wooho <woohobi@gmail.com>
2022-05-25 12:50:44 +0200

    Add GET post api

--- src/main/java/escaper/backend/repository/post/PostRepositoryCustom.java
+++ src/main/java/escaper/backend/repository/post/PostRepositoryCustom.java
@@ -4,8 +4,10 @@ import escaper.backend.entity.PostResponseDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
+import java.util.List;
+
 
 public interface PostRepositoryCustom {
 
-    Page<PostResponseDto> findPagePost(Pageable pageable);
+    List<PostResponseDto> findPagePost();
 }
--- src/main/java/escaper/backend/repository/post/PostRepositoryImpl.java
+++ src/main/java/escaper/backend/repository/post/PostRepositoryImpl.java
@@ -1,18 +1,36 @@
 package escaper.backend.repository.post;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import escaper.backend.entity.Post;
 import escaper.backend.entity.PostResponseDto;
+import escaper.backend.entity.QPostResponseDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+
+import static escaper.backend.entity.QPost.post;
+import static escaper.backend.entity.theme.QTheme.theme;
 
 @RequiredArgsConstructor
+@Slf4j
 public class PostRepositoryImpl implements PostRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public Page<PostResponseDto> findPagePost(Pageable pageable) {
-        return null;
+    public List<PostResponseDto> findPagePost() {
+        List<PostResponseDto> results = queryFactory
+                .select(new QPostResponseDto(
+                        post.id,
+                        post.title,
+                        post.content,
+                        theme.name
+                ))
+                .from(post)
+                .leftJoin(post.theme, theme)
+                .fetch();
+        long total = results.size();
+        return results;
     }
 }
