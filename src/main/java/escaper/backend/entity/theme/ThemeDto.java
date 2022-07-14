ba28bb9567a39850836619c0cfe2d6870a85e513
wooho <woohobi@gmail.com>
2022-07-14 17:33:38 +0200

    UPDATE dto 프로퍼티 추가

--- src/main/java/escaper/backend/entity/theme/ThemeDto.java
+++ src/main/java/escaper/backend/entity/theme/ThemeDto.java
@@ -7,9 +7,15 @@ public class ThemeDto {
 
     private Long id;
     private String name;
+    private String genre;
+    private Integer rate;
+    private String imageURL;
 
     public ThemeDto(Theme theme) {
         this.id = theme.getId();
         this.name = theme.getName();
+        this.genre = theme.getGenre();
+        this.rate = theme.getRate();
+        this.imageURL = theme.getImageURL();
     }
 }
