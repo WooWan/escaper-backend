b03a13c944cb272263053f43f51497968dd62956
wooho <woohobi@gmail.com>
2022-07-14 17:34:36 +0200

    Feat 인기 theme api 추가

--- src/main/java/escaper/backend/controller/ThemeController.java
+++ src/main/java/escaper/backend/controller/ThemeController.java
@@ -1,5 +1,7 @@
 package escaper.backend.controller;
 
+import escaper.backend.entity.theme.Theme;
+import escaper.backend.entity.theme.ThemeDto;
 import escaper.backend.entity.theme.ThemeSearchDto;
 import escaper.backend.repository.theme.ThemeRepository;
 import escaper.backend.service.ThemeService;
@@ -10,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
+import static java.util.stream.Collectors.*;
 
 @Getter
 @RequiredArgsConstructor
@@ -22,5 +26,21 @@ public class ThemeController {
     @GetMapping("api/themes")
     public List<ThemeSearchDto> searchTheme(@RequestParam(required = false) String cafe) {
         return themeRepository.searchTheme(cafe);
+    }
+
+    @GetMapping("/api/themes/popular")
+    public List<ThemeDto> findPopularTheme() {
+        List<Theme> results = themeRepository.findPopularTheme();
+        return results.stream()
+                .map(ThemeDto::new)
+                .collect(toList());
+    }
+
+    @GetMapping("/api/themes/genre")
+    public List<ThemeDto> findThemeByGenre(String genre) {
+        List<Theme> results = themeRepository.findThemeByGenre(genre);
+        return results.stream()
+                .map(ThemeDto::new)
+                .collect(toList());
     }
 }
