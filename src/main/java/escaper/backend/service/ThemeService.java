41a387dcb7902486c9cff795516cb7409c6fc05f
wooho <woohobi@gmail.com>
2022-07-23 16:03:14 +0200

    Add 장르 중복제거

--- src/main/java/escaper/backend/service/ThemeService.java
+++ src/main/java/escaper/backend/service/ThemeService.java
@@ -1,13 +1,30 @@
 package escaper.backend.service;
 
+import escaper.backend.entity.theme.ThemeTypes;
+import escaper.backend.repository.theme.ThemeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Transactional
 @Service
 public class ThemeService {
+
+    private final ThemeRepository themeRepository;
+
+    public List<ThemeTypes> getThemeTypes() {
+        List<ThemeTypes> result = themeRepository.findThemeTypes();
+        Set<String> nameSet = new HashSet<>();
+        return result.stream()
+                .filter(theme -> nameSet.add(theme.getGenre()))
+                .collect(Collectors.toList());
+    }
 
 
 }
