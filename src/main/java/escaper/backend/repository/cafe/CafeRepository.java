bddccd6844c5cf913661b27c21fe4cc115ce0f78
wooho <woohobi@gmail.com>
2022-08-16 00:19:05 +0900

    Chore 식별자 Long으로 변경

--- src/main/java/escaper/backend/repository/cafe/CafeRepository.java
+++ src/main/java/escaper/backend/repository/cafe/CafeRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface CafeRepository extends JpaRepository<Cafe, Integer>, CafeRepositoryCustom {
+public interface CafeRepository extends JpaRepository<Cafe, Long>, CafeRepositoryCustom {
 
     @Query("SELECT DISTINCT c.address.city FROM Cafe c")
     List<String> searchCities();
