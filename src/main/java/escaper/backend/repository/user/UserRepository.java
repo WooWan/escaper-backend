e23de652aeffc0fb8e1939a2481f995c1431bbba
wooho <woohobi@gmail.com>
2022-08-20 17:49:17 +0900

    Remove User

--- src/main/java/escaper/backend/repository/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package escaper.backend.repository.user;
-
-import escaper.backend.entity.user.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUserId(String userId);
-}
