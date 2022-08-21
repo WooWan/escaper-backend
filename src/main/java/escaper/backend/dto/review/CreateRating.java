bfe33aac566e5b021216d9d42271cb036e4639f2
wooho <woohobi@gmail.com>
2022-08-21 20:00:53 +0900

    Feat 방탈출 테마 리뷰 남기기

---
+++ src/main/java/escaper/backend/dto/review/CreateRating.java
@@ -0,0 +1,9 @@
+package escaper.backend.dto.review;
+
+import lombok.Data;
+
+@Data
+public class CreateRating {
+
+    private Double rating;
+}
--- src/main/java/escaper/backend/entity/review/CreateReview.java
+++ src/main/java/escaper/backend/entity/review/CreateReview.java
@@ -1,21 +1,17 @@
 package escaper.backend.entity.review;
 
 import lombok.Getter;
-import org.springframework.lang.Nullable;
 
 @Getter
 public class CreateReview {
 
-    private Long memberId;
-    private Double rate;
-    @Nullable
     private String content;
 
     public Review toEntity() {
         return Review.builder()
-                .rate(rate)
                 .content(content)
-                .like(0)
+                .rate(0.0)
+                .likes(0)
                 .build();
     }
 }
