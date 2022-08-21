b8cfbe26004229da13c48e94ec6bfe7547d9d449
wooho <woohobi@gmail.com>
2022-08-21 19:53:38 +0900

    Add 공통 예외처리

---
+++ src/main/java/escaper/backend/error/dto/ErrorCode.java
@@ -0,0 +1,21 @@
+package escaper.backend.error.dto;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public enum ErrorCode {
+
+    //member
+    NOT_FOUND_MEMBER("M001", "Not found member", HttpStatus.BAD_REQUEST);
+
+    private final String code;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    ErrorCode(String code, String message, HttpStatus httpStatus) {
+        this.code = code;
+        this.message = message;
+        this.httpStatus = httpStatus;
+    }
+}
