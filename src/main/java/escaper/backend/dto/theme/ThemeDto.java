9536db729030b7de29928af24f626a74bd6da814
wooho <woohobi@gmail.com>
2022-08-22 16:14:03 +0900

    Rename from rate to rating

--- src/main/java/escaper/backend/dto/theme/ThemeDto.java
+++ src/main/java/escaper/backend/dto/theme/ThemeDto.java
@@ -9,14 +9,14 @@ public class ThemeDto {
     private Long id;
     private String name;
     private String genre;
-    private Double rate;
+    private Double rating;
     private String imageURL;
 
     public ThemeDto(Theme theme) {
         this.id = theme.getId();
         this.name = theme.getName();
         this.genre = theme.getGenre();
-        this.rate = theme.getRate();
+        this.rating = theme.getRating();
         this.imageURL = theme.getImageURL();
     }
 }
