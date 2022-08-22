c1e255a665f14135f94671d67722dfc4f3902a49
wooho <woohobi@gmail.com>
2022-08-22 20:45:10 +0900

    Feat 유저 평점 남기기

--- src/main/java/escaper/backend/dto/review/CreateRating.java
+++ src/main/java/escaper/backend/dto/review/CreateRating.java
@@ -1,9 +1,16 @@
 package escaper.backend.dto.review;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CreateRating {
 
     private Double rating;
+
+    public CreateRating(Double rating) {
+        this.rating = rating;
+    }
 }
