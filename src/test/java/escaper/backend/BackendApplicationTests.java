cc624731d43121fe7cce72741f4523de3a7f3eb7
wooho <woohobi@gmail.com>
2022-08-22 20:45:31 +0900

    Chore 테스트 공통 yaml 변경

--- src/test/java/escaper/backend/BackendApplicationTests.java
+++ src/test/java/escaper/backend/BackendApplicationTests.java
@@ -3,12 +3,10 @@ package escaper.backend;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest
 @ContextConfiguration(initializers = ConfigDataApplicationContextInitializer.class)
-@ActiveProfiles("it")
 class BackendApplicationTests {
 
 	@Test
