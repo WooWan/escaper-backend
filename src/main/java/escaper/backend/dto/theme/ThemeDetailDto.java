2db6f71f756e0e3777354ffb549f5d24a7dd9dd7
wooho <woohobi@gmail.com>
2022-08-22 16:14:12 +0900

    Rename from rate to rating

--- src/main/java/escaper/backend/dto/theme/ThemeDetailDto.java
+++ src/main/java/escaper/backend/dto/theme/ThemeDetailDto.java
@@ -1,34 +1,34 @@
 package escaper.backend.dto.theme;
 
 import com.querydsl.core.annotations.QueryProjection;
-import escaper.backend.entity.theme.Theme;
-import lombok.Builder;
 import lombok.Data;
 
 @Data
 public class ThemeDetailDto {
 
-    private Long id;
+    private Long themeId;
     private String name;
     private String genre;
     private Integer timeLimitation;
     private Integer appropriatedPeople;
-    private Double rate;
+    private Double themeRating;
     private Integer cost;
     private String imageURL;
 
+    private Long cafeId;
     private String cafeName;
 
     @QueryProjection
-    public ThemeDetailDto(Long id, String name, String genre, Integer timeLimitation, Integer appropriatedPeople, Double rate, Integer cost, String imageURL, String cafeName) {
-        this.id = id;
+    public ThemeDetailDto(Long themeId, String name, String genre, Integer timeLimitation, Integer appropriatedPeople, Double themeRating, Integer cost, String imageURL, Long cafeId, String cafeName) {
+        this.themeId = themeId;
         this.name = name;
         this.genre = genre;
         this.timeLimitation = timeLimitation;
         this.appropriatedPeople = appropriatedPeople;
-        this.rate = rate;
+        this.themeRating = themeRating;
         this.cost = cost;
         this.imageURL = imageURL;
+        this.cafeId = cafeId;
         this.cafeName = cafeName;
     }
 }
