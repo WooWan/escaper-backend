18e233def03f267f6c1cb4474bfc6cb7887f24b1
wooho <woohobi@gmail.com>
2022-08-24 12:45:37 +0900

    Test 테스트 변경 삭제

--- src/test/java/escaper/backend/repository/post/PostRepositoryImplTest.java
+++ src/test/java/escaper/backend/repository/post/PostRepositoryImplTest.java
@@ -1,12 +1,6 @@
 package escaper.backend.repository.post;
 
 import escaper.backend.config.QuerydslTestConfiguration;
-import escaper.backend.entity.Post;
-import escaper.backend.entity.PostResponseDto;
-import escaper.backend.entity.User;
-import escaper.backend.entity.theme.Theme;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
@@ -14,41 +8,15 @@ import org.springframework.context.annotation.Import;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
-import java.util.List;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
 @Transactional
 @Import(QuerydslTestConfiguration.class)
 class PostRepositoryImplTest {
 
     @Autowired
-    private PostRepositoryImpl postRepository;
-    @Autowired
     EntityManager em;
 
-    @Test
-    @DisplayName("모든 post가 페치된다")
-    public void 페치_post() {
-        //given
-        Theme theme1 = new Theme("theme1");
-        Theme theme2 = new Theme("theme2");
-        em.persist(theme1);
-        em.persist(theme2);
-        User user1 = new User("user1");
-        em.persist(user1);
-        Post post1 = new Post("post1", "content1", user1, theme1);
-        Post post2 = new Post("post2", "content2", user1, theme1);
-        Post post3 = new Post("post3", "content3", user1, theme2);
-        em.persist(post1);
-        em.persist(post2);
-        em.persist(post3);
-        //when
-        List<PostResponseDto> results = postRepository.findPagePost();
 
-        //then
-        assertThat(results.size()).isEqualTo(3);
-    }
 
 }
--- src/test/java/escaper/backend/repository/theme/ThemeRepositoryTest.java
+++ src/test/java/escaper/backend/repository/theme/ThemeRepositoryTest.java
@@ -1,22 +1,12 @@
 package escaper.backend.repository.theme;
 
 import escaper.backend.config.QuerydslTestConfiguration;
-import escaper.backend.entity.cafe.Cafe;
-import escaper.backend.entity.theme.Theme;
-import escaper.backend.entity.theme.ThemeSearchCondition;
-import escaper.backend.entity.theme.ThemeSearchDto;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 @DataJpaTest
 @Transactional
@@ -27,19 +17,5 @@ class ThemeRepositoryTest {
     @Autowired
     EntityManager entityManager;
 
-    @Test
-    @DisplayName("조건에 맞는 항목만 서치되어야 한다")
-    public void 테마서치_조건_테스트() {
-        //given
-        Cafe cafe = new Cafe("cafeA");
-        entityManager.persist(cafe);
 
-        Theme theme = new Theme("theme1", cafe);
-        entityManager.persist(theme);
-        //when
-        String condition = "cafeA";
-        List<ThemeSearchDto> results = themeRepository.searchTheme(condition);
-        //then
-        assertThat(results).extracting("themeName").contains("theme1");
-    }
 }
