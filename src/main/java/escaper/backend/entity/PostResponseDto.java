3189b8a302e797b13e5bf6d976e0ff8698a8b826
wooho <woohobi@gmail.com>
2022-08-24 12:38:28 +0900

    Feat 게시글 댓글 dto

--- src/main/java/escaper/backend/entity/PostResponseDto.java
+++ src/main/java/escaper/backend/entity/PostResponseDto.java
@@ -1,31 +1,36 @@
 package escaper.backend.entity;
 
+import escaper.backend.entity.comment.CommentDto;
 import escaper.backend.entity.post.Post;
 import escaper.backend.dto.theme.ThemeDto;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import lombok.extern.slf4j.Slf4j;
 import java.util.List;
 
 import static java.util.stream.Collectors.*;
 
 @Data
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class PostResponseDto {
 
     private Long id;
     private String title;
     private String content;
-    private List<ThemeDto> themeList;
+    private List<CommentDto> comments;
+    private ThemeDto themeDto;
 
     public PostResponseDto(Post post) {
         this.id = post.getId();
         this.title = post.getTitle();
         this.content = post.getContent();
-        this.themeList = post.getThemeList().stream()
-                .map(ThemeDto::new)
+        this.themeDto = new ThemeDto(post.getTheme());
+        this.comments = post.getComments().stream()
+                .map(CommentDto::new)
                 .collect(toList());
+
     }
 
 }
