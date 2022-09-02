4881d21a61b034c75678759c4ef7bfe18d46831e
wooho <woohobi@gmail.com>
2022-09-02 19:01:06 +0900

    Update 카페이름 추가

--- src/main/java/escaper/backend/dto/theme/ThemeDto.java
+++ src/main/java/escaper/backend/dto/theme/ThemeDto.java
@@ -6,14 +6,16 @@ import lombok.Data;
 @Data
 public class ThemeDto {
 
-    private Long id;
+    private Long themeId;
+    private String cafeName;
     private String name;
     private String genre;
     private Double rating;
     private String imageURL;
 
     public ThemeDto(Theme theme) {
-        this.id = theme.getId();
+        this.themeId = theme.getId();
+        this.cafeName = theme.getCafe().getName();
         this.name = theme.getName();
         this.genre = theme.getGenre();
         this.rating = theme.getRating();
