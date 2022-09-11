cea1f59b53a395213fd1c1838c082788d0ebcee5
wooho <woohobi@gmail.com>
2022-09-11 23:26:57 +0900

    Update Cafe dto에 id 응답 추가

--- src/main/java/escaper/backend/service/cafe/CafeConverter.java
+++ src/main/java/escaper/backend/service/cafe/CafeConverter.java
@@ -1,16 +1,13 @@
 package escaper.backend.service.cafe;
 
-import escaper.backend.dto.cafe.CafeDto;
 import escaper.backend.dto.cafe.CafeResponse;
-import escaper.backend.dto.theme.ThemeResponse;
 import escaper.backend.entity.cafe.Cafe;
-
-import java.util.stream.Collectors;
 
 public class CafeConverter {
 
     public static CafeResponse toCafeResponse(Cafe cafe){
         return CafeResponse.builder()
+                .id(cafe.getId())
                 .name(cafe.getName())
                 .address(cafe.getAddress())
                 .build();
